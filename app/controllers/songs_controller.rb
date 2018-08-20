9e1d7bcee4f16c82e194a3cd2c7a94fa6624d1ea
Yina Zhu <yinagit@gmail.com>
2018-08-20 21:25:11 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -44,7 +44,12 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      artist = Artist.find_by(:artist_id)
+      if artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      end
+    end
   end
 
   def update
